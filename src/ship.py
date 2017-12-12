289cd98daeebc827b25f6b7c893dd5b8d6df6fd7
succoDiPompelmo <zenari12@gmail.com>
2017-12-12 17:46:11 +0100

    Change ship image path due to a file movement

--- src/ship.py
+++ src/ship.py
@@ -8,7 +8,7 @@ class Ship():
         self.ai_settings = ai_settings
 
         # Load the ship image and get its rect
-        self.image = pygame.image.load('ship.bmp')
+        self.image = pygame.image.load('../img/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = self.screen.get_rect()
 
